7a2d07c549a4f4875253c28516f29ea1ba642d76
josiahdc <josiah.chapman@gmail.com>
2022-09-30 16:56:20 -0700

    birth

---
+++ notebook.sh
@@ -0,0 +1,3 @@
+# get eden
+cd ~ || exit
+git clone https://github.com/euclidean-dreams/eden.git
---
+++ services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo cp ~/eden/services/analogorium.service /etc/systemd/system/analogorium.service
+sudo cp ~/eden/services/cosmographer.service /etc/systemd/system/cosmographer.service
+sudo cp ~/eden/services/promenade.service /etc/systemd/system/promenade.service
+
+sudo systemctl enable analogorium.service
+sudo systemctl enable cosmographer.service
+sudo systemctl enable promenade.service
