e030fbaa0ca19ddb1865cab1b6ab7036e2bc5a89
josiah <josiah.chapman@gmail.com>
2023-11-01 19:25:08 -0700

    (~)

--- services.sh
+++ services.sh
@@ -7,3 +7,6 @@ sudo cp ~/eden/services/promenade.service /etc/systemd/system/promenade.service
 sudo systemctl enable analogorium.service
 sudo systemctl enable cosmographer.service
 sudo systemctl enable promenade.service
+
+sudo cp ~/sjofn.service /etc/systemd/system/sjofn.service
+sudo systemctl enable sjofn.service
