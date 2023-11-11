cb3129c080e98aea8f5474fa0f65681865a5288d
josiah <josiah.chapman@gmail.com>
2023-11-10 23:46:43 -0800

    (~)

--- services.sh
+++ services.sh
@@ -5,11 +5,6 @@ sudo cp ~/conjure/services/cosmographer.service /etc/systemd/system/cosmographer
 sudo cp ~/conjure/services/luciferon.service /etc/systemd/system/luciferon.service
 sudo cp ~/conjure/services/promenade.service /etc/systemd/system/promenade.service
 
-sudo cp /home/josiah/euclidean-dreams/analogorium/cmake-build-sjofn/analogorium /usr/local/analogorium
-sudo cp /home/josiah/euclidean-dreams/cosmographer/cmake-build-sjofn/cosmographer /usr/local/cosmographer
-sudo cp /home/josiah/euclidean-dreams/luciferon/cmake-build-release-sjofn/luciferon /usr/local/luciferon
-sudo cp /home/josiah/euclidean-dreams/promenade/cmake-build-sjofn/promenade /usr/local/promenade
-
 sudo systemctl enable analogorium.service
 sudo systemctl enable cosmographer.service
 sudo systemctl enable luciferon.service
