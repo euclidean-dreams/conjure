3e9e5b898c7e96ebf03f2b372a2038e8e1b5285c
josiah <josiah.chapman@gmail.com>
2023-11-10 22:23:17 -0800

    (~)

--- services.sh
+++ services.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo cp ~/eden/services/analogorium.service /etc/systemd/system/analogorium.service
-sudo cp ~/eden/services/cosmographer.service /etc/systemd/system/cosmographer.service
-sudo cp ~/eden/services/promenade.service /etc/systemd/system/promenade.service
+sudo cp ~/conjure/services/analogorium.service /etc/systemd/system/analogorium.service
+sudo cp ~/conjure/services/cosmographer.service /etc/systemd/system/cosmographer.service
+sudo cp ~/conjure/services/luciferon.service /etc/systemd/system/luciferon.service
+sudo cp ~/conjure/services/promenade.service /etc/systemd/system/promenade.service
 
 sudo systemctl enable analogorium.service
 sudo systemctl enable cosmographer.service
+sudo systemctl enable luciferon.service
 sudo systemctl enable promenade.service
 
 sudo cp ~/sjofn.service /etc/systemd/system/sjofn.service
